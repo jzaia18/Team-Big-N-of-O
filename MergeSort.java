b59ea632d84cf9cd12fde4c170fd4ce4b3faaf00
Jake Zaia <jzaia@stuy.edu>
2017-02-14 08:31:14 -0500

    Did a thing

--- MergeSort.java
+++ MergeSort.java
@@ -1,7 +1,7 @@
-// Maggie Xia
+// Team Big N of o: Maggie Xia, Joshel Xiedeng, Jake Zaia
 // APCS2 pd1
-// HW6 -- Step 1: Split, Step 2: ?, Step 3: Sorted!. . .
-// 2017-02-13
+// HW07 -- What Does the Data Say?  
+// 2017-02-14
 
 /*======================================
   class MergeSort
@@ -34,14 +34,17 @@ public class MergeSort {
 	
 	int x = 0;//counter for array a
 	int y = 0;//counter for array b
+	
+	int al = a.length;
+	int bl = b.length;
 
-	while ( x < a.length || y < b.length ) {
+	while ( x < al || y < bl ) {
 	    
-	    if ( x == a.length ) {//if array a is empty
+	    if ( x == al ) {//if array a is empty
 		retArr[ x + y ] = b[y];
 		y += 1;
 	    }
-	    else if ( y == b.length ) {//if array b is empty
+	    else if ( y == bl ) {//if array b is empty
 		retArr[ x + y ] = a[x];
 		x += 1;
 	    }
@@ -114,20 +117,32 @@ public class MergeSort {
     }
     //---------------------------------------------------
 
+    public static void populate(int []  a){
+	int al = a.length;
+	for (int i = 0; i < al; i++){
+	    a[i] = (int) 101 * Math.random();
+	}
+    }
 
     //main method for testing
     public static void main( String [] args ) {
 
+	/*
 	int[] arr0 = {0};
-	int[] arr1 = {1};
-	int[] arr2 = {1,2};
-	int[] arr3 = {3,4};
-	int[] arr4 = {1,2,3,4};
-	int[] arr5 = {4,3,2,1};
-	int[] arr6 = {9,42,17,63,0,512,23};
-	int[] arr7 = {9,42,17,63,0,9,512,23,9};
+	int[] arr1 = new int [10];
+	int[] arr2 = new int [100];
+	int[] arr3 = new int [1000];
+	int[] arr4 = new int [10000];
+	int[] arr5 = new int [100000];
 
-	System.out.println("\nTesting mess-with-array method...");
+	populate(arr1);
+	populate(arr2);
+	populate(arr3);
+	populate(arr4);
+	populate(arr5);
+	populate(arr6);*/
+
+	/*System.out.println("\nTesting mess-with-array method...");
 	printArray( arr3 );
 	mess(arr3);
 	printArray( arr3 );
@@ -142,7 +157,7 @@ public class MergeSort {
 	printArray( sort( arr4 ) );
 	printArray( sort( arr5 ) );
 	printArray( sort( arr6 ) );
-	printArray( sort( arr7 ) );
+	printArray( sort( arr7 ) );*/
 
     }//end main()
 
