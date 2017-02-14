8e535b93ae52cdb9e33757c83ca50baa5e52455c
Jake Zaia <jzaia@stuy.edu>
2017-02-14 15:07:16 -0500

    In-class changes

--- MergeSortTester.java
+++ MergeSortTester.java
@@ -27,8 +27,29 @@ public class MergeSortTester {
      *  YOUR APPARATUS FOR GENERATING EXECUTION 
      *  TIME DATA...>
      ******************************/
-    public static void main( String[] args ) {
 
+    public static void populate(int []  a){
+	int al = a.length;
+	for (int i = 0; i < al; i++){
+	    a[i] = (int) 101 * Math.random();
+	}
+    }
+
+
+    public static void main( String[] args ) {
+	int[] arr0 = {0};
+	int[] arr1 = new int [10];
+	int[] arr2 = new int [100];
+	int[] arr3 = new int [1000];
+	int[] arr4 = new int [10000];
+	int[] arr5 = new int [100000];
+
+	populate(arr1);
+	populate(arr2);
+	populate(arr3);
+	populate(arr4);
+	populate(arr5);
+	populate(arr6);
     }//end main
 
 }//end class
