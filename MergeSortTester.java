d78b0410ad51db3f4a3861f55fa1b875acde289d
MoogieMoo <yxia1@stuy.edu>
2017-02-15 01:12:26 -0500

    Update MergeSortTester.java

--- MergeSortTester.java
+++ MergeSortTester.java
@@ -9,12 +9,12 @@
   O(n*log(n))
   Mean execution times for dataset of size n:
   Batch size: <# of times each dataset size was run>
-  n=1       time: 10
-  n=10      time: 1487136630551 --> 57132
-  n=100     time: 1487136677494 --> 46943
-  n=1000    time: 1487136719313 --> 41819
-  n=10000   time: 1487136774863 --> 55550
-  n=100000  time: 1487136814856 --> 39993
+  n=1       time: 0
+  n=10      time: 1
+  n=100     time: 1
+  n=1000    time: 4
+  n=10000   time: 7
+  n=100000  time: 34
   n=<huge>  time: n*log(n)
   ANALYSIS:
   <INSERT YOUR RESULTS ANALYSIS HERE>
@@ -41,25 +41,25 @@ public class MergeSortTester {
         
     long beginTime = System.currentTimeMillis();
         
-	int[] arr0 = {0};
-	int[] arr1 = new int [10];
-	int[] arr2 = new int [100];
-	int[] arr3 = new int [1000];
-	int[] arr4 = new int [10000];
-	int[] arr5 = new int [100000];
+	//int[] arr0 = {0};
+	//int[] arr1 = new int [10];
+	//int[] arr2 = new int [100];
+	//int[] arr3 = new int [1000];
+	//int[] arr4 = new int [10000];
+	//int[] arr5 = new int [100000];
 
-	populate(arr1);
-	populate(arr2);
-	populate(arr3);
-	populate(arr4);
-	populate(arr5);
+	//populate( arr1 );
+	//populate( arr2 );
+	//populate( arr3 );
+	//populate( arr4 );
+	//populate( arr5 );
 	
-	MergeSort.sort( arr0 );
-	MergeSort.sort( arr1 );
-	MergeSort.sort( arr2 );
-	MergeSort.sort( arr3 );
-	MergeSort.sort( arr4 );
-	MergeSort.sort( arr5 );
+	//MergeSort.sort( arr0 );
+	//MergeSort.sort( arr1 );
+	//MergeSort.sort( arr2 );
+	//MergeSort.sort( arr3 );
+	//MergeSort.sort( arr4 );
+	//MergeSort.sort( arr5 );
 	
 	long endTime = System.currentTimeMillis();
     long difference = endTime - beginTime;
